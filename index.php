b39c67f9a493ae88ad70f27d6b23ce9fc8d792c3
Duvall Danganan <duvalldanganan@gmail.com>
2024-07-10 15:01:11 +0800

    db tsql

--- index.php
+++ index.php
@@ -1,5 +1,22 @@
 <?php 
 
-echo "Hello World!";
-echo "Phen G!"
+    $serverName = "sqlsvrphp.database.windows.net";
+    $connectionOptions = array(
+        "Database" => "account",
+        "Uid" => "sqladmin",
+        "PWD" => "P@ssw0rd123!"
+    );
+
+    $conn = sqlsrv_connect($serverName, $connectionOptions);
+    $tsql = "SELECT firstName, lastName FROM dbo.employees";
+    $getResults = sqlsrv_query ($conn, $tsql);
+    echo ("Reading data from table" . PHP_EOL);
+    if ($getResults == FALSE)
+        echo (sqlsrv_errors());
+    while ($row = sqlsrv_fetch_array($getResults, SQLSRV_FETCH_ASSOC)){
+        echo ($row['firstName'] . " " . $row['lastName'] . PHP_EOL);
+    }
+    sqlsrv_free_stmt($getResults);
+    
+
 ?>
